e6572bec5107a4b0372680bf1935c599f2bb3889
DESKTOP-QFUJBHK\Marina <marinasolovieva189@gmail.com>
2023-08-06 21:09:05 +0300

    springBoot+security

---
+++ src/main/java/com/solovyeva/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.solovyeva.controller;
+
+import com.solovyeva.dto.Product;
+import com.solovyeva.entity.UserInfo;
+import com.solovyeva.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo){
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+}
---
+++ src/test/java/com/solovyeva/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solovyeva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
